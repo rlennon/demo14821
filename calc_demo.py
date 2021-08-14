0323e6a0ce847cca1936314cd7ba5a7a6d4bc6d5
Ruth Lennon <Ruth.Lennon@lyit.ie>
2021-08-14 23:14:35 +0100

    Fix typo

--- calc_demo.py
+++ calc_demo.py
@@ -2,5 +2,5 @@ print("Calculator")
 num_1 = int(input('Enter num 1: '))
 num_2 = int(input('Enter num 2: '))
 
-print('{1} + {2} = '.format(number_1, number_2))
-print(number_1 + number_2)
+print('{1} + {2} = '.format(num_1, num_2))
+print(num_1 + num_2)
